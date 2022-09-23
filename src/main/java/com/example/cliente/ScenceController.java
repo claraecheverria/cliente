a78d5ba7c6c28ba2ca16efc59040c5e8853b32cc
IgnacioSoler1 <isoler@correo.um.edu.uy>
2022-09-23 12:43:05 -0300

    agregue UI - hay que proobar

--- src/main/java/com/example/cliente/ScenceController.java
+++ src/main/java/com/example/cliente/ScenceController.java
@@ -1,11 +1,15 @@
 package com.example.cliente;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.springframework.stereotype.Controller;
 
 import java.awt.*;
@@ -18,6 +22,9 @@ public class ScenceController {
     private Parent root;
 
     private Text NombreUsuario;
+
+    @FXML
+    private BarChart<String, Integer> chart;
 
     public void switchToScence1(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader();
@@ -34,6 +41,13 @@ public class ScenceController {
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scence = new Scene(root);
         stage.setScene(scence);
+        stage.initStyle(StageStyle.UNDECORATED);
+        XYChart.Series<String,Integer> series_01 = new XYChart.Series<>();
+        series_01.getData().add(new XYChart.Data<>("Mon", 60));
+        series_01.getData().add(new XYChart.Data<>("Tue", 40));
+        series_01.getData().add(new XYChart.Data<>("Wen", 80));
+        series_01.getData().add(new XYChart.Data<>("Thr", 100));
+        chart.getData().add(series_01);
         stage.show();
 
     }
