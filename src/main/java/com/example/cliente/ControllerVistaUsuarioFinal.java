54bf820927e9f26277c8654aee6cd96189d01503
IgnacioSoler1 <isoler@correo.um.edu.uy>
2022-10-07 17:45:51 -0300

    vista configuracion usuario final

--- src/main/java/com/example/cliente/ControllerVistaUsuarioFinal.java
+++ src/main/java/com/example/cliente/ControllerVistaUsuarioFinal.java
@@ -1,13 +1,18 @@
 package com.example.cliente;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 import org.springframework.stereotype.Controller;
 
+import java.awt.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,18 +24,30 @@ public class ControllerVistaUsuarioFinal implements Initializable {
 
     @FXML
     private BarChart<String, Integer> chart;
+    @FXML
+    private Button BotonConfiguracion;
+
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         XYChart.Series series1 = new XYChart.Series();
         series1.setName("2003");
-        series1.getData().add(new XYChart.Data("Lunes", 2323));
-        series1.getData(). add(new XYChart.Data("Martes" , 4333));
-        series1.getData().add(new XYChart.Data("Miercoles" ,10000));
-        series1.getData().add(new XYChart.Data("Jueves",35000));
-        series1.getData().add(new XYChart.Data("Viernes",12000));
+        series1.getData().add(new XYChart.Data("Lunes", 80));
+        series1.getData(). add(new XYChart.Data("Martes" , 60));
+        series1.getData().add(new XYChart.Data("Miercoles" ,45));
+        series1.getData().add(new XYChart.Data("Jueves",100));
+        series1.getData().add(new XYChart.Data("Viernes",50));
 
         chart.getData() . addAll(series1);
     }
+
+    public void switchToConfiguracion(javafx.event.ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent root = fxmlLoader.load(HelloApplication.class.getResourceAsStream("AgregarEmpleadoEmpresa.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scence = new Scene(root);
+        stage.show();
+    }
+
 }
