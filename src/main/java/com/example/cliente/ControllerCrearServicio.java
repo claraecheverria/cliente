39aac8b740cc3a0abbae9dbe65db76f7991a172a
IgnacioSoler1 <isoler@correo.um.edu.uy>
2022-10-14 15:42:22 -0300

    atributos tipo

--- src/main/java/com/example/cliente/ControllerCrearServicio.java
+++ src/main/java/com/example/cliente/ControllerCrearServicio.java
@@ -54,7 +54,7 @@ public class ControllerCrearServicio {
         Horarios.clear();
         Cupos.clear();
 
-        Servicio nuevoServicio = new Servicio(nombre, precio, horarios,cupos);
+        Servicio nuevoServicio = new Servicio(nombre, precio, horarios,tipo);
 
         HttpResponse<JsonNode> response2 = Unirest.post("http://localhost:8080/empresa/crearEmpresa")
                 .header("accept", "application/json")
