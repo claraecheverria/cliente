6ac6f127c11ef80e632392200fc768f15864fc8b
Clara <cecheverria@correo.um.edu.uy>
2022-10-18 12:29:38 -0300

    Constructores y getters y setters reserva

--- src/main/java/com/example/cliente/Model/Reserva.java
+++ src/main/java/com/example/cliente/Model/Reserva.java
@@ -16,4 +16,67 @@ public class Reserva {
     private Cancha cancha;
 
     private List<UserEmpleado> usuariosInvitados;
+
+    //CONSTRUCTORES
+
+    public Reserva() {
+    }
+
+    public Reserva(LocalDate fecha, LocalTime horaInicio, LocalTime horaFin, Cancha cancha, List<UserEmpleado> usuariosInvitados) {
+        this.fecha = fecha;
+        this.horaInicio = horaInicio;
+        this.horaFin = horaFin;
+        this.cancha = cancha;
+        this.usuariosInvitados = usuariosInvitados;
+    }
+
+    //GETTERS Y SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalTime getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(LocalTime horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public LocalTime getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(LocalTime horaFin) {
+        this.horaFin = horaFin;
+    }
+
+    public Cancha getCancha() {
+        return cancha;
+    }
+
+    public void setCancha(Cancha cancha) {
+        this.cancha = cancha;
+    }
+
+    public List<UserEmpleado> getUsuariosInvitados() {
+        return usuariosInvitados;
+    }
+
+    public void setUsuariosInvitados(List<UserEmpleado> usuariosInvitados) {
+        this.usuariosInvitados = usuariosInvitados;
+    }
 }
