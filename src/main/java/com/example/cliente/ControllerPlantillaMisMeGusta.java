a1c69342f638e0e006b6c2a25237025e83e6c74e
IgnacioSoler1 <isoler@correo.um.edu.uy>
2022-10-21 20:59:14 -0300

    PAGE1 actualizada

--- src/main/java/com/example/cliente/ControllerPlantillaMisMeGusta.java
+++ src/main/java/com/example/cliente/ControllerPlantillaMisMeGusta.java
@@ -1,5 +1,6 @@
 package com.example.cliente;
 
+import com.example.cliente.Model.Servicio;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import org.springframework.stereotype.Controller;
@@ -15,6 +16,18 @@ public class ControllerPlantillaMisMeGusta {
     private Label Direccion;
     @FXML
     private Label Descripcion;
+    @FXML
+    private Label Nombre;
 
+    public void setData(Servicio servicio){
+//        javafx.scene.image.Image image = parseByteToImage(servicio.imagen)
+//        Image.setImage(image);
+//        setServicioEste(servicio);//REPASR SI ESTA BIEN
+        Nombre.setText(servicio.getKey().getNombre());
+        Direccion.setText(servicio.getCentroDeportivoServicio().getDireccion());
+        Descripcion.setText(servicio.getDescripcion());
+        HorarioInicio.setText(servicio.getHoraInicio());
+        HorarioFin.setText(servicio.getHoraFin());
 
+    }
 }
