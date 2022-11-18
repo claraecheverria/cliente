e94b6a2582f838cc213270a884ea21e45b532bc2
IgnacioSoler1 <isoler@correo.um.edu.uy>
2022-11-18 19:28:15 -0300

    un poco de todo

--- src/main/java/com/example/cliente/ControllerCrearServicio.java
+++ src/main/java/com/example/cliente/ControllerCrearServicio.java
@@ -34,7 +34,7 @@ public class ControllerCrearServicio implements Initializable {
     private Stage stage;
     private Scene scence;
     ObservableList<String> tipoList = FXCollections.observableArrayList("GYM", "Futbol", "Pisina", "Yoga", "Spinning", "Tennis", "Spinning");
-    ObservableList<String> horariosList = FXCollections.observableArrayList("0:00","1:00","2:00","3:00","4:00","5:00","6:00","7:00","8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00","24:00");
+    ObservableList<String> horariosList = FXCollections.observableArrayList("0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24");
     @FXML
     private TextField Nombre;
     @FXML
@@ -73,6 +73,8 @@ public class ControllerCrearServicio implements Initializable {
 
     @FXML
     private CheckBox DiaViernes;
+    @FXML
+    private Label lable;
 
     private String imagen = null;
 
@@ -120,6 +122,8 @@ public class ControllerCrearServicio implements Initializable {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        lable.setText("Imagen ingresada correctamente");
+
     }
 
     public void guradarDatos(){
--- src/main/java/com/example/cliente/ControllerEmpresa.java
+++ src/main/java/com/example/cliente/ControllerEmpresa.java
@@ -39,4 +39,15 @@ public class ControllerEmpresa {
         stage.show();
     }
 
+    public void cerrarSecion(javafx.event.ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent root = fxmlLoader.load(HelloApplication.class.getResourceAsStream("LoginPage2.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        String css = this.getClass().getResource("StilePage1.css").toExternalForm();
+        scence = new Scene(root);
+        scence.getStylesheets().add(css);
+        stage.setScene(scence);
+        stage.show();
+    }
+
 }
